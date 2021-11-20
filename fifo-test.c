eda0420863a36ca14d63fd2cee32db927588074a
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2021-11-20 13:14:37 +0100

    Added generics

--- fifo-test.c
+++ fifo-test.c
@@ -1,8 +1,9 @@
-// FIFO test
+// Channel test
 
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdint.h>
 
 // comment next line to enable assertions
 //#define NDEBUG
@@ -12,20 +13,15 @@
 // Scalar
 ////////////////////////////////////////////////////////////////////////
 
-typedef signed long   Signed;
-typedef unsigned long Unsigned;
-typedef double        Real;
-typedef void*         Pointer;
-
 typedef union {
-	Signed   i;
-	Unsigned u;
-	Real     r;
-	Pointer  p;
+	int64_t  i;
+	uint64_t u;
+	double   r;
+	void*    p;
 } Scalar __attribute__((__transparent_union__));
 
 ////////////////////////////////////////////////////////////////////////
-// FIFO
+// Channel
 ////////////////////////////////////////////////////////////////////////
 
 #define N 10
@@ -34,49 +30,102 @@ typedef struct {
 	size_t front;
 	size_t rear;
 	size_t size;
-	Scalar buffer[N+1];
-} FIFO;
+	Scalar buffer[N+1]; // TODO: use malloc
+} Channel;
 
 #define ALWAYS __attribute__((always_inline))
 
-static ALWAYS inline bool q_empty(FIFO* self)
+static inline void chn_init(Channel* self, size_t size);
+static inline void chn_destroy(Channel* self);
+static ALWAYS inline bool chn_empty(Channel* self);
+static ALWAYS inline bool chn_full(Channel* self);
+static ALWAYS inline void chn_snd_(Channel* self, Scalar x);
+static ALWAYS inline void chn_rcv(Channel* self, Scalar* x);
+//#define chn_cast(scalar,variable) 
+
+// Channel life
+
+static inline void chn_init(Channel* self, size_t size)
+{
+	self->front = self->rear = 0;
+	self->size = size+1;
+	// TODO: malloc
+	assert(chn_empty(self));
+	assert(!chn_full(self));
+}
+
+static inline void chn_destroy(Channel* self)
+{
+	; // TODO
+}
+
+// Predicates
+
+static ALWAYS inline bool chn_empty(Channel* self)
 {
 	return self->front == self->rear;
 }
 
-static ALWAYS inline bool q_full(FIFO* self)
+static ALWAYS inline bool chn_full(Channel* self)
 {
 	return ((self->front+1) % self->size) == self->rear;
 }
 
-static inline void q_init(FIFO* self)
-{
-	self->front = self->rear = 0;
-	self->size = sizeof(self->buffer) / sizeof(*self->buffer);
-	assert(q_empty(self));
-	assert(!q_full(self));
-}
+// Send & Receive
 
-static inline void q_destroy(FIFO* self)
+static ALWAYS inline void chn_snd_(Channel* self, Scalar x)
 {
-	;
-}
-
-static inline void q_put(FIFO* self, Scalar x)
-{
-	assert(!q_full(self));
+	assert(!chn_full(self));
 	self->buffer[self->front] = x;
 	self->front = (self->front+1) % self->size;
-	assert(!q_empty(self));
+	assert(!chn_empty(self));
 }
 
-static inline void q_get(FIFO* self, Scalar* x)
+#define chn_snd(C,S) chn_snd_((C), _Generic((S),\
+	_Bool: (uint64_t)(S),\
+	char: (uint64_t)(S),\
+	signed char: (int64_t)(S),\
+	unsigned char: (uint64_t)(S),\
+	signed short int: (int64_t)(S),\
+	unsigned short int: (uint64_t)(S),\
+	signed int: (int64_t)(S),\
+	unsigned int: (uint64_t)(S),\
+	signed long int: (int64_t)(S),\
+	unsigned long int: (uint64_t)(S),\
+	signed long long int: (int64_t)(S),\
+	unsigned long long int: (uint64_t)(S),\
+	float: (double)(S),\
+	double: (double)(S),\
+	long double: (double)(S),\
+	default: (void*)(intptr_t)(S))/* assume pointer */\
+)
+
+static ALWAYS inline void chn_rcv(Channel* self, Scalar* x)
 {
-	assert(!q_empty(self));
+	assert(!chn_empty(self));
 	*x = self->buffer[self->rear];
 	self->rear = (self->rear+1) % self->size;
-	assert(!q_full(self));
+	assert(!chn_full(self));
 }
+
+#define chn_cast(S,V) _Generic(V,\
+	_Bool: (_Bool)(S).u,\
+	char: (char)(S).u,\
+	signed char: (signed char)(S).i,\
+	unsigned char: (unsigned char)(S).u,\
+	signed short int: (signed short int)(S).i,\
+	unsigned short int: (unsigned short int)(S).u,\
+	signed int: (signed int)(S).i,\
+	unsigned int: (unsigned int)(S).u,\
+	signed long int: (signed long int)(S).i,\
+	unsigned long int: (unsigned long int)(S).u,\
+	signed long long int: (signed long long int)(S).i,\
+	unsigned long long int: (unsigned long long int)(S).u,\
+	float: (float)(S).r,\
+	double: (double)(S).r,\
+	long double: (long double)(S).r,\
+	default: (void*)(S).p\
+)
 
 #undef ALWAYS
 
@@ -86,29 +135,31 @@ static inline void q_get(FIFO* self, Scalar* x)
 
 int main(int argc, char* argv[])
 {
-	static_assert(sizeof(Signed)==sizeof(Unsigned));
-	static_assert(sizeof(Signed)==sizeof(Real));
-	static_assert(sizeof(Signed)==sizeof(Pointer));
-	static_assert(sizeof(Signed)==sizeof(Scalar));
-	static_assert(sizeof(Signed)==8);
+	static_assert(sizeof(double)==sizeof(int64_t));
+	static_assert(sizeof(double)==sizeof(uint64_t));
+	static_assert(sizeof(double)==sizeof(void*));
+	static_assert(sizeof(Scalar)==8);
 
-	FIFO queue;
+	Channel chn;
 
-	q_init(&queue);
+	chn_init(&chn, N);
 
-	for (Signed i='0'; !q_full(&queue); ++i) {
-		q_put(&queue, i);
+	for (char c='0'; !chn_full(&chn); ++c) {
+		chn_snd(&chn, c);
 	}
-	assert(q_full(&queue));
+	assert(chn_full(&chn));
 
 	Scalar s;
-	while (!q_empty(&queue)) {
-		q_get(&queue, &s);
-		putchar(s.i);
+	for (char c; !chn_empty(&chn);) {
+		chn_rcv(&chn, &s);
+		c = chn_cast(s, c); // chn_set(c, s); ???
+		putchar(c);
 	}
-	assert(q_empty(&queue));
-	q_destroy(&queue);
+	assert(chn_empty(&chn));
+
 	putchar('\n');
+
+	chn_destroy(&chn);
 
 	return 0;
 }
