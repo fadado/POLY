8217989d5234048e642713fcfb5690234607c762
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2021-11-24 12:03:27 +0100

    Rescued types

--- channel.h
+++ channel.h
@@ -8,7 +8,6 @@
 #define CHANNEL_H
 
 #include <stdbool.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <threads.h>
 
@@ -20,12 +19,23 @@
 // Interface
 ////////////////////////////////////////////////////////////////////////
 
+typedef signed long long   Signed;
+typedef unsigned long long Unsigned;
+typedef double             Real;
+typedef void*              Pointer;
+
 typedef union {
-	int64_t  i;
-	uint64_t u;
-	double   r;
-	void*    p;
-} Scalar __attribute__((__transparent_union__));
+	Signed   i;
+	Unsigned u;
+	Real     r;
+	Pointer  p;
+} Scalar
+__attribute__((__transparent_union__));
+
+static_assert(sizeof(Real)==sizeof(Signed));
+static_assert(sizeof(Real)==sizeof(Unsigned));
+static_assert(sizeof(Real)==sizeof(Pointer));
+static_assert(sizeof(Scalar)==8);
 
 typedef struct {
 	unsigned front;
@@ -42,32 +52,31 @@ typedef struct {
 
 static        inline int  chn_init(Channel* self, unsigned capacity);
 static        inline void chn_destroy(Channel* self);
-static ALWAYS inline bool _chn_empty(Channel* self);
-static ALWAYS inline bool _chn_full(Channel* self);
 static ALWAYS inline int  chn_send_(Channel* self, Scalar x);
 static ALWAYS inline int  chn_receive(Channel* self, Scalar* x);
 
 // Accept any scalar type
-#define chn_send(CHANNEL,SCALAR) chn_send_((CHANNEL), _Generic((SCALAR),\
-	_Bool: (uint64_t)(SCALAR),\
-	char: (uint64_t)(SCALAR),\
-	signed char: (int64_t)(SCALAR),\
-	unsigned char: (uint64_t)(SCALAR),\
-	signed short int: (int64_t)(SCALAR),\
-	unsigned short int: (uint64_t)(SCALAR),\
-	signed int: (int64_t)(SCALAR),\
-	unsigned int: (uint64_t)(SCALAR),\
-	signed long int: (int64_t)(SCALAR),\
-	unsigned long int: (uint64_t)(SCALAR),\
-	signed long long int: (int64_t)(SCALAR),\
-	unsigned long long int: (uint64_t)(SCALAR),\
-	float: (double)(SCALAR),\
-	double: (double)(SCALAR),\
-	long double: (double)(SCALAR),\
-	default: (void*)(intptr_t)(SCALAR)))/*assume pointer*/\
+#define chn_send(CHANNEL,EXPRESSION) chn_send_((CHANNEL), _Generic((EXPRESSION),\
+	Scalar: (EXPRESSION),\
+	_Bool: (Unsigned)(EXPRESSION),\
+	char: (Unsigned)(EXPRESSION),\
+	signed char: (Signed)(EXPRESSION),\
+	unsigned char: (Unsigned)(EXPRESSION),\
+	signed short int: (Signed)(EXPRESSION),\
+	unsigned short int: (Unsigned)(EXPRESSION),\
+	signed int: (Signed)(EXPRESSION),\
+	unsigned int: (Unsigned)(EXPRESSION),\
+	signed long int: (Signed)(EXPRESSION),\
+	unsigned long int: (Unsigned)(EXPRESSION),\
+	signed long long int: (Signed)(EXPRESSION),\
+	unsigned long long int: (Unsigned)(EXPRESSION),\
+	float: (Real)(EXPRESSION),\
+	double: (Real)(EXPRESSION),\
+	long double: (Real)(EXPRESSION),\
+	default: (Pointer)(Unsigned)(EXPRESSION)))
 
-// Cast an Scalar to the variable type
-#define chn_cast(SCALAR,VARIABLE) _Generic((VARIABLE),\
+// Cast an Scalar to the expression type
+#define chn_cast(SCALAR,EXPRESSION) _Generic((EXPRESSION),\
 	_Bool: (_Bool)(SCALAR).u,\
 	char: (char)(SCALAR).u,\
 	signed char: (signed char)(SCALAR).i,\
@@ -90,53 +99,6 @@ static ALWAYS inline int  chn_receive(Channel* self, Scalar* x);
 ////////////////////////////////////////////////////////////////////////
 
 //
-// Channel life
-//
-static inline int chn_init(Channel* self, unsigned capacity)
-{
-	static_assert(sizeof(double)==sizeof(int64_t));
-	static_assert(sizeof(double)==sizeof(uint64_t));
-	static_assert(sizeof(double)==sizeof(void*));
-	static_assert(sizeof(Scalar)==8);
-
-	int err;
-#	define catch(e)	if ((err=(e)) != thrd_success) goto onerror
-
-	self->count = self->front = self->back = 0;
-	self->size  = (capacity == 0 ? 1 : capacity);
-
-	self->buffer = calloc(self->size, sizeof(Scalar));
-	if (!self->buffer) return thrd_nomem;
-
-	//assert(_chn_empty(self));
-	//assert(!_chn_full(self));
-
-	catch(mtx_init(&self->monitor, mtx_plain));
-	catch(cnd_init(&self->nonempty));
-	catch(cnd_init(&self->nonfull));
-
-	return thrd_success;
-
-onerror:
-#	undef catch
-	return err;
-}
-
-static inline void chn_destroy(Channel* self)
-{
-	//assert(self->count == 0)
-
-	free(self->buffer);
-	mtx_destroy(&self->monitor);
-	cnd_destroy(&self->nonempty);
-	cnd_destroy(&self->nonfull);
-
-	// sanitize
-	self->count = self->front = self->back = self->size = 0;
-	self->buffer = (Scalar*)0;
-}
-
-//
 // Predicates
 //
 static ALWAYS inline bool _chn_empty(Channel* self)
@@ -150,32 +112,108 @@ static ALWAYS inline bool _chn_full(Channel* self)
 }
 
 //
+// Channel life
+//
+static inline int chn_init(Channel* self, unsigned capacity)
+{
+	self->count = self->front = self->back = 0;
+	self->size  = (capacity == 0 ? 1 : capacity);
+
+	self->buffer = calloc(self->size, sizeof(Scalar));
+	if (self->buffer == (Scalar*)0) return thrd_nomem;
+
+	int err, eN=0;
+#	define catch(X)	if ((++eN,err=(X))!=thrd_success) goto onerror
+
+	catch (mtx_init(&self->monitor, mtx_plain)); // eN == 1
+	catch (cnd_init(&self->nonempty));           // eN == 2
+	catch (cnd_init(&self->nonfull));            // eN == 3
+
+	return thrd_success;
+
+onerror:
+#	undef catch
+	assert(err != thrd_success);
+	assert(1 <= eN && eN <= 3);
+	switch (eN) {
+		case 3: cnd_destroy(&self->nonempty);
+		case 2: mtx_destroy(&self->monitor);
+		case 1: free(self->buffer);
+		default: return err;
+	}
+}
+
+static inline void chn_destroy(Channel* self)
+{
+	assert(self->count == 0);
+
+	free(self->buffer);
+	mtx_destroy(&self->monitor);
+	cnd_destroy(&self->nonempty);
+	cnd_destroy(&self->nonfull);
+
+	// sanitize
+	self->count = self->front = self->back = self->size = 0;
+	self->buffer = (Scalar*)0;
+}
+
+//
 // Send & Receive
 //
 static ALWAYS inline int chn_send_(Channel* self, Scalar x)
 {
-	assert(!_chn_full(self));
+	int err;
+#	define catch(X)	if ((err=(X))!=thrd_success)
+
+	catch (mtx_lock(&self->monitor)) { return err; }
+
+	if (_chn_full(self)) {
+		catch (cnd_wait(&self->nonfull, &self->monitor)) {
+			mtx_destroy(&self->monitor);
+			return err;
+		}
+	}
 
 	self->buffer[self->front] = x;
 	self->front = (self->front+1) % self->size;
 	++self->count;
 
-	assert(!_chn_empty(self));
+	catch (mtx_unlock(&self->monitor)) {
+		cnd_signal(&self->nonempty);
+		return err;
+	}
+	catch (cnd_signal(&self->nonempty)) { return err; }
 
 	return thrd_success;
+#	undef catch
 }
 
 static ALWAYS inline int chn_receive(Channel* self, Scalar* x)
 {
-	assert(!_chn_empty(self));
+	int err;
+#	define catch(X)	if ((err=(X))!=thrd_success)
+
+	catch (mtx_lock(&self->monitor)) { return err; }
+
+	if (_chn_empty(self)) {
+		catch (cnd_wait(&self->nonempty, &self->monitor)) {
+			mtx_destroy(&self->monitor);
+			return err;
+		}
+	}
 
 	*x = self->buffer[self->back];
 	self->back = (self->back+1) % self->size;
 	--self->count;
 
-	assert(!_chn_full(self));
+	catch (mtx_unlock(&self->monitor)) {
+		cnd_signal(&self->nonfull);
+		return err;
+	}
+	catch (cnd_signal(&self->nonfull)) { return err; }
 
 	return thrd_success;
+#	undef catch
 }
 
 #undef ALWAYS
--- fifo-test.c
+++ fifo-test.c
@@ -1,30 +1,38 @@
 // Channel test
 
-#include <errno.h>	// ENOMEM
-#include <string.h>	// strerror
-#include <stdio.h>	// IO
-#include <stdlib.h>	// abort
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <error.h>
 
-#include "channel.h" // stdbool, stdint, stdlib, threads
+#include "channel.h"
+
+// comment next line to enable assertions
+//#define NDEBUG
+#include <assert.h>
+
+////////////////////////////////////////////////////////////////////////
+// FIFO test
+////////////////////////////////////////////////////////////////////////
 
 #define N 10
 
 int main(int argc, char* argv[])
 {
 	int err;
-#	define catch(e)	if ((err=(e)) != thrd_success) goto onerror
+#	define catch(X)	if ((err=(X))!=thrd_success) goto onerror
 
 	Channel chn;
-	catch(chn_init(&chn, N));
+	catch (chn_init(&chn, N));
 
 	for (int i=0; i < N; ++i) {
-		catch(chn_send(&chn, '0'+i));
+		catch (chn_send(&chn, '0'+i));
 	}
 
 	Scalar s;
 	for (int i=0; i < N; ++i) {
-		catch(chn_receive(&chn, &s));
-		char c = chn_cast(s, i);
+		catch (chn_receive(&chn, &s));
+		char c = chn_cast(s, '@');
 		putchar(c);
 	}
 
@@ -36,14 +44,22 @@ int main(int argc, char* argv[])
 
 onerror:
 #	undef catch
+	enum { internal_error };
+	static const char const* ename[] = {
+		[thrd_success] = "thrd_success",
+		[thrd_nomem] = "thrd_nomem",
+		[thrd_busy] = "thrd_busy",
+		[thrd_error] = "thrd_error",
+		[thrd_timedout] = "thrd_timedout",
+	};
+	assert(err != thrd_success);
 	switch (err) {
 		case thrd_nomem:
-			fprintf(stderr, "%s\n", strerror(ENOMEM));
-			break;
-		case thrd_busy: // TODO
+		case thrd_busy:
 		case thrd_error:
 		case thrd_timedout:
-		default: abort();
+			error(EXIT_FAILURE, ENOMEM, "%s", ename[err]);
+		default: assert(internal_error);
 	}
 	return EXIT_FAILURE;
 }
